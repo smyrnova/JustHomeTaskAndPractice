09a5355bcbe573d8335c76ba70cd6a35a919abbf
smyrnova <aleksandra.smyrnova@gmail.com>
2022-09-10 17:51:20 +0300

    Task 6-7

--- src/homework/HW10.java
+++ src/homework/HW10.java
@@ -86,6 +86,36 @@ public class HW10 {
         return count;
     }
 
+    public static boolean task6FindWord(String str) {
+        if (str != null) {
+
+            return str.contains("Java");
+        }
+
+        return false;
+    }
+
+
+    public static String task7Concat(String str) {
+        if (str != null) {
+            if (!str.isEmpty()) {
+                String dot = ".";
+                String kavi4ki = "\"";
+
+                str = str.trim();
+                str = kavi4ki.concat(str);
+                str = str.concat(dot);
+                str = str.concat(kavi4ki);
+
+                return str;
+            } else {
+
+                return str;
+            }
+        }
+        return "Smt went wrong."; //если null
+    }
+
 
     public static void main(String[] arg) {
         /** Написать метод, который принимает на вход строку.
@@ -199,6 +229,83 @@ public class HW10 {
         System.out.println(task5CountAa(str2));
         System.out.println(task5CountAa(str4));
         System.out.println(task5CountAa(str3));
+
+        line();
+
+        /**6.
+         * Напишите метод, который принимает на вход текст и проверяет, содержится ли в тексте хотя бы одно слово Java.
+         * Test Data:
+         * “As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support
+         * (LTS) versions. Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January
+         * 2019 for commercial use, although it will otherwise still support Java 8 with public updates for personal
+         * use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are still
+         * receiving security and other upgrades.” → true
+         *
+         * “As a decrepit father takes delight
+         * To see his active child do deeds of youth,
+         * So I, made lame by fortune’s dearest spite,
+         * Take all my comfort of thy worth and truth.
+         * For whether beauty, birth, or wealth, or wit,
+         * Or any of these all, or all, or more,
+         * Entitled in thy parts do crownèd sit,
+         * I make my love engrafted to this store.
+         * So then I am not lame, poor, nor despised,
+         * Whilst that this shadow doth such substance give
+         * That I in thy abundance am sufficed,
+         * And by a part of all thy glory live.
+         * Look what is best, that best I wish in thee.
+         * This wish I have; then ten times happy me.” → false
+         */
+        printTaskNumber(2, 6);
+
+        str1 = "As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support\n" +
+                "         * (LTS) versions. Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January\n" +
+                "         * 2019 for commercial use, although it will otherwise still support Java 8 with public updates for personal\n" +
+                "         * use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are still\n" +
+                "         * receiving security and other upgrades.";
+        str2 = "As a decrepit father takes delight\n" +
+                "         * To see his active child do deeds of youth,\n" +
+                "         * So I, made lame by fortune’s dearest spite,\n" +
+                "         * Take all my comfort of thy worth and truth.\n" +
+                "         * For whether beauty, birth, or wealth, or wit,\n" +
+                "         * Or any of these all, or all, or more,\n" +
+                "         * Entitled in thy parts do crownèd sit,\n" +
+                "         * I make my love engrafted to this store.\n" +
+                "         * So then I am not lame, poor, nor despised,\n" +
+                "         * Whilst that this shadow doth such substance give\n" +
+                "         * That I in thy abundance am sufficed,\n" +
+                "         * And by a part of all thy glory live.\n" +
+                "         * Look what is best, that best I wish in thee.\n" +
+                "         * This wish I have; then ten times happy me.";
+        str3 = null;
+        str4 = "";
+
+        System.out.println(task6FindWord(str1));
+        System.out.println(task6FindWord(str2));
+        System.out.println(task6FindWord(str4));
+        System.out.println(task6FindWord(str3));
+
+        line();
+
+        /**7.
+         *  Напишите метод, который принимает на вход строку, и добавляет Кавычки в начале строки, точку и кавычки в
+         *  конце строки с помощью метода concat()
+         * Test Data:
+         * “One” → ““One.””
+         * “    TWO  “ → ““TWO.””
+         */
+        printTaskNumber(2, 7);
+
+        str1 = "One";
+        str2 = "    TWO  ";
+        str3 = null;
+        str4 = "";
+
+        System.out.println(task7Concat(str1));
+        System.out.println(task7Concat(str2));
+        System.out.println(task7Concat(str4));
+        System.out.println(task7Concat(str3));
+
 
     }
 }
